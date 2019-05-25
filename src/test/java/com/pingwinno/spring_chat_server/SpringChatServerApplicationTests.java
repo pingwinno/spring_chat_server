cd248493308efaf400adf1b34169df3dc8529fef
pingwinno <pingwinno@gmail.com>
2019-05-26 01:13:27 +0300

    initial version

---
+++ src/test/java/com/pingwinno/spring_chat_server/SpringChatServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pingwinno.spring_chat_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringChatServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
